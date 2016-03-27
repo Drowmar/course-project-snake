821a0c7200207172eaf70ebf0d8ce57ba46c00f9
Сманцер Марина < >
2016-03-27 17:32:30 +0300

    Mmm

--- snake/snake/snake.c
+++ snake/snake/snake.c
@@ -2,10 +2,13 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Windows.h>
+
 
 void main(void)
 {
-	int check, n, m, i, j, fi, coordinate_x, coordinate_y, player, number, flag;
+	int check, n, m, i, j, fi, coordinate_x, coordinate_y, player, number, flag, head_x, head_y, way_x, way_y;
+	char press;
 	srand(time(NULL));
 	char** arr = NULL;
 	//проверка на ввод
@@ -75,14 +78,76 @@ void main(void)
 		}
 	}
 	printf("\n");
+
+	head_x = 0 + rand() % n;
+	head_y = 0 + rand() % m;
+	arr[head_x][head_y] = 'o';
+
+	way_x = 0;
+	way_y = 1;
+	while (head_x < n && head_y < m && head_x > 0 && head_y > 0)
+	{
+		//вывод
+		system("cls");
+		printf("  ");
+		for (j = 1; j <= m; j++)
+		{
+			printf("%3d", j);
+		}
+		for (i = 0; i < n; i++)
+		{
+			printf("\n%2d", i + 1);
+			for (j = 0; j < m; j++)
+			{
+				printf("%3c", arr[i][j]);
+			}
+		}
+		printf("\n");
+		//встреча с едой
+		if (arr[head_x][head_y] == '*')
+		{
+			coordinate_x = 0 + rand() % n;
+			coordinate_y = 0 + rand() % m;
+			arr[coordinate_x][coordinate_y] = '*';
+		}
+		//поворот
+		_sleep(1000); 
+		if (kbhit() != 0)
+		{
+			press = (char)getch();
+			if (press == 'w')
+			{
+				way_x = -1;
+				way_y = 0;
+			}
+			if (press == 'a')
+			{
+				way_x = 0;
+				way_y = -1;
+			}
+			if (press == 'd')
+			{
+				way_x = 0;
+				way_y = 1;
+			}
+			if (press == 's')
+			{
+				way_x = 1;
+				way_y = 0;
+			}
+		}
+		arr[head_x][head_y] = '.';
+		head_x = head_x + way_x;
+		head_y = head_y + way_y;
+		if (head_y >= m)
+			break;
+		arr[head_x][head_y] = 'o';
+	}
 	//освобождение памяти
 	for (i = 0; i < n; i++)
 	{
 		free(arr[i]);
 	}
 	free(arr);
-
-	getch();
-	_sleep(600);
 	return 0;
 }
